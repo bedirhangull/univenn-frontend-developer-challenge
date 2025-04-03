1819dcc0ddb99fb80d28c5dc714643ac12e9eca6
bedirhangull <gu1bedirhann@gmail.com>
2025-04-03 13:05:27 +0300

    redux toolkit and redux persist installation completed.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import './index.css'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App'
+import { store, persistor } from '@/store/store'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </BrowserRouter>
 )
